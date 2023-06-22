050f23085103a7f37b0246989d254fdd9460bbea
Andrew <Andrew@CMBNB1120194.COMBIPHAR.COM>
2023-06-22 15:52:49 +0700

    Change colors and added signup invalid messages

--- client/src/pages/SignupPage/SignupPage.js
+++ client/src/pages/SignupPage/SignupPage.js
@@ -22,6 +22,47 @@ const SignupPage = (props) => {
         }
     }
 
+    const checkUsername = () => {
+        if (username.length === 0) return "";
+        if (username.length > 16) {
+            return "username can only be up to 16 characters";
+        } else if (!username.match(/^[0-9a-z]+$/)) {
+            return "username must be alphanumeric";
+        } else {
+            return "";
+        }
+    }
+
+    const checkEmail = () => {
+        if (email.length === 0) return "";
+        if (!email
+        .toLowerCase()
+        .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )) {
+            return "please enter a valid email";
+        } else {
+            return "";
+        }
+    }
+
+    const checkPassword = () => {
+        if (password.length === 0) return "";
+        if (password.length < 8) {
+            return "password must be at least 8 characters";
+        } else if (password === password.toLowerCase()) {
+            return "password must contain at least 1 uppercase";
+        } else if (password === password.toUpperCase()) {
+            return "password must contain at least 1 lowercase";
+        } else if (!/\d/.test(password)) {
+            return "password must contain a number";
+        } else if (!/[ `!@#$%^&*()_+\-=\]{};':"\\|,.<>?~]/.test(password)) {
+            return "password must contain a special character";
+        } else {
+            return "";
+        }
+    }
+
     return (
         <div className={styles.login}>
             <div className={styles.container}>
@@ -32,19 +73,32 @@ const SignupPage = (props) => {
                         type="text"
                         onChange={(e) => setUsername(e.target.value)}
                         value={username}
+                        style={  checkUsername() !== "" ? {
+                            border: '1px solid red'
+                        } : {}}
                     />
+                    <p className={styles.invalid}>{checkUsername(username)}</p>
                     <label htmlFor="">Email:</label>
                     <input
                         type="email"
                         onChange={(e) => setEmail(e.target.value)}
                         value={email}
+                        style={  checkEmail() !== "" ? {
+                            border: '1px solid #ff8164',
+                            color: '#ff8164'
+                        } : {}}
                     />
+                    <p className={styles.invalid}>{checkEmail(email)}</p>
                     <label htmlFor="">Password:</label>
                     <input
                         type="password"
                         onChange={(e) => setPassword(e.target.value)}
                         value={password}
+                        style={  checkPassword() !== "" ? {
+                            border: '1px solid red'
+                        } : {}}
                     />
+                    <p className={styles.invalid}>{checkPassword(password)}</p>
                     <button disabled={isLoading}>Sign Up</button>
                     {error && <div className={styles.error}>{error}</div>}
                 </form>
