9bad916e663a0ba21934a0815b33b0feac548622
Andrew Janong <andrewjanong@gmail.com>
2023-06-26 00:43:49 +0700

    Issues in navbar current page highlight

--- client/src/components/NavBar/Section.js
+++ client/src/components/NavBar/Section.js
@@ -4,7 +4,7 @@ import styles from "./Section.module.css";
 const Section = (props) => {
 
     return (
-        <div className={`${styles.section} ${props.path ? styles.currentPage : ''}`}>
+        <div className={`${styles.section}`}>
             <img src={require(`../../icons/${props.section}.png`)} alt="" />
             <p>{props.section}</p>
         </div>
